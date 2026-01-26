62a1f0f7344188a7f65d462b81c94ac8ab491733
David (Favour) Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2026-01-26 23:05:59 +0100

    update on manifest file and removal of favicon.ico to icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Coachme - CoachMe",
   description: "CoachMe Application",
+  manifest: "/manifest.json",
 };
 
 export default function CoachmeLayout({
